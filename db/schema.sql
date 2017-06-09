c536b5387dc0aad0cc7cac818c4de3e8a5cf93d6
luisdajer9 <luisdajer9@gmail.com>
2017-06-08 21:08:45 -0400

    sequelize

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+# Create the burgers table
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOL DEFAULT false,
+PRIMARY KEY (id)
+);
